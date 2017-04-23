ce8a4322202caf7b29a22560c4ae7bc6f7bb4f96
bogdan <bodia005@gmail.com>
2017-04-23 21:31:02 +0300

    finished mpi program

--- CourseWorkProgram2/CourseWorkProgram2/main.cpp
+++ CourseWorkProgram2/CourseWorkProgram2/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
+#include <limits.h>
 #include "mpi.h"
 
 using namespace std;
 
 const int N = 6;
-const int P = 6;
-const int H = N / P;
 
 void inputMatrix(int matrix[N][N]);
 void outputMatrix(int matrix[N][N]);
@@ -13,6 +12,12 @@ void inputVector(int vector[N]);
 void outputVector(int vector[N]);
 
 void ckeckSize(int r, int s);
+
+int maxNumber(int vector[N], int start, int end);
+
+void sendMatrixPart(int matrix[N][N], int start, int end, int dest, int tag);
+void recvMatrixPart(int matrix[N][N], int start, int end, int source, int tag, MPI_Status status);
+
 
 bool isNull(int V[N], int M1[N][N], int M2[N][N], int M3[N][N], int M4[N][N]);
 
@@ -32,6 +37,9 @@ int main(int argc, char* argv[])
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	ckeckSize(rank, size);
+
+	int P = size;
+	int H = N / P;
 
 	//input data
 	if (rank == 0)
@@ -136,22 +144,123 @@ int main(int argc, char* argv[])
 		MPI_Recv(Z, N, MPI_INT, rank - P / 3, msgTag, MPI_COMM_WORLD, &status);
 	}
 
-	cout << "Tread N_" << rank << " - " << isNull(Z, MB, MO, MC, MK) << endl;
+	int startIndex = rank*H;
+	int endIndex = (rank + 1)*H;
 
-	/*if (rank == 0)
+	//Find max element
+	int a_i = maxNumber(Z, startIndex, endIndex);
+
+	MPI_Allreduce(&a_i, &a, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+
+	//Calc MA_H
+	for (int i = startIndex; i < endIndex; i++)
 	{
-	cout << "Thread N_" << rank << endl;
-	cout << "Vector Z:" << endl;
-	outputVector(Z);
-	cout << "Matrix MB:" << endl;
-	outputMatrix(MB);
-	cout << "Matrix MO:" << endl;
-	outputMatrix(MO);
-	cout << "Matrix MC:" << endl;
-	outputMatrix(MC);
-	cout << "Matrix MK:" << endl;
-	outputMatrix(MK);
-	}*/
+		for (int j = 0; j < N; j++)
+		{
+			int M1 = 0, M2 = 0;
+
+			for (int k = 0; k < N; k++)
+			{
+				M1 += MB[i][k] * MC[k][j];
+				M2 += MO[i][k] * MK[k][j];
+			}
+
+			int resultValue = M1 + a * M2;
+			MA[i][j] = resultValue;
+		}
+	}
+
+
+	if (rank > 2 * P / 3 - 1 && rank < P)
+	{
+		sendMatrixPart(MA, startIndex, endIndex, rank - P / 3, msgTag);
+	}
+	if (rank > P / 3 - 1 && rank < 2 * P / 3)
+	{
+		int tempStart = (rank + P / 3) * H;
+		int tempEnd = (rank + 1 + P / 3) * H;
+
+		recvMatrixPart(MA, tempStart, tempEnd, rank + P / 3, msgTag, status);
+
+		sendMatrixPart(MA, startIndex, endIndex, rank - P / 3, msgTag);
+		sendMatrixPart(MA, tempStart, tempEnd, rank - P / 3, msgTag);
+	}
+	if (rank == 0)
+	{
+		int tempStart = (P / 3) * H;
+		int tempEnd = (P / 3 + 1) * H;
+
+		int secondTempStart = (2 * P / 3) * H;
+		int secondTempEnd = (2 * P / 3 + 1) * H;
+
+		recvMatrixPart(MA, tempStart, tempEnd, rank + P / 3, msgTag, status);
+		recvMatrixPart(MA, secondTempStart, secondTempEnd, rank + P / 3, msgTag, status);
+
+		sendMatrixPart(MA, startIndex, endIndex, rank + 1, msgTag);
+		sendMatrixPart(MA, tempStart, tempEnd, rank + 1, msgTag);
+		sendMatrixPart(MA, secondTempStart, secondTempEnd, rank + 1, msgTag);
+	}
+	if (rank > 0 && rank < P / 3 - 1)
+	{
+		int start1 = 0;
+		int end1 = rank * H;
+
+		int start2 = (P / 3) * H;
+		int end2 = (P / 3 + rank) * H;
+
+		int start3 = (2 * P / 3) * H;
+		int end3 = (2 * P / 3 + rank) * H;
+
+		recvMatrixPart(MA, start1, end1, rank - 1, msgTag, status);
+		recvMatrixPart(MA, start2, end2, rank - 1, msgTag, status);
+		recvMatrixPart(MA, start3, end3, rank - 1, msgTag, status);
+
+		int tempStart = (P / 3 + rank) * H;
+		int tempEnd = (P / 3 + rank + 1) * H;
+
+		int secondTempStart = (2 * P / 3 + rank) * H;
+		int secondTempEnd = (2 * P / 3 + rank + 1) * H;
+
+		recvMatrixPart(MA, tempStart, tempEnd, rank + P / 3, msgTag, status);
+		recvMatrixPart(MA, secondTempStart, secondTempEnd, rank + P / 3, msgTag, status);
+
+
+		sendMatrixPart(MA, start1, endIndex, rank + 1, msgTag);
+		sendMatrixPart(MA, start2, tempEnd, rank + 1, msgTag);
+		sendMatrixPart(MA, start3, secondTempEnd, rank + 1, msgTag);
+	}
+	if (rank == P / 3 - 1)
+	{
+		int start1 = 0;
+		int end1 = rank * H;
+
+		int start2 = (P / 3) * H;
+		int end2 = (P / 3 + rank) * H;
+
+		int start3 = (2 * P / 3) * H;
+		int end3 = (2 * P / 3 + rank) * H;
+
+		recvMatrixPart(MA, start1, end1, rank - 1, msgTag, status);
+		recvMatrixPart(MA, start2, end2, rank - 1, msgTag, status);
+		recvMatrixPart(MA, start3, end3, rank - 1, msgTag, status);
+
+		int tempStart = (P / 3 + rank) * H;
+		int tempEnd = (P / 3 + rank + 1) * H;
+
+		int secondTempStart = (2 * P / 3 + rank) * H;
+		int secondTempEnd = (2 * P / 3 + rank + 1) * H;
+
+		recvMatrixPart(MA, tempStart, tempEnd, rank + P / 3, msgTag, status);
+		recvMatrixPart(MA, secondTempStart, secondTempEnd, rank + P / 3, msgTag, status);
+	}
+
+
+	if (rank == P / 3 - 1)
+	{
+		cout << "Result MA:" << endl;
+		outputMatrix(MA);
+	}
+
 
 	MPI_Finalize();
 	return 0;
@@ -201,47 +310,48 @@ void ckeckSize(int r, int s)
 	{
 		if (r == 0)
 			cout << "For correct program work the count of threads must be more than 6" <<
-			"and multiple of three. Please make sure that you input correct data"
-			<< endl << "Your threads' size is " << s << endl;
+			"and multiple of three. Please make sure that you input correct data!" << endl <<
+			"Your threads' size is " << s << endl;
+		MPI_Finalize();
+		exit(-1);
+	}
+	if (N % 3 != 0)
+	{
+		if (r == 0)
+			cout << "The dimension of the arrays must be a multiple of three." <<
+			"Please make sure that you input correct data!" << endl <<
+			"Current dimension is " << N << endl;
 		MPI_Finalize();
 		exit(-1);
 	}
 }
 
-bool isNull(int V[N], int M1[N][N], int M2[N][N], int M3[N][N], int M4[N][N]) {
+int maxNumber(int vector[N], int start, int end)
+{
+	int result = INT_MIN;
+	for (int i = start; i < end; i++)
+	{
+		if (vector[i] > result)
+		{
+			result = vector[i];
+		}
+	}
 
-	for (int i = 0; i < N; i++)
-	{
-		if (V[i] != 1) return true;
-	}
+	return result;
+}
 
-	for (int i = 0; i < N; i++)
+void sendMatrixPart(int matrix[N][N], int start, int end, int dest, int tag)
+{
+	for (int i = start; i < end; i++)
 	{
-		for (int j = 0; j < N; j++)
-		{
-			if (M1[i][j] != 1) return true;
-		}
+		MPI_Send(matrix[i], N, MPI_INT, dest, tag, MPI_COMM_WORLD);
 	}
-	for (int i = 0; i < N; i++)
+}
+
+void recvMatrixPart(int matrix[N][N], int start, int end, int source, int tag, MPI_Status status)
+{
+	for (int i = start; i < end; i++)
 	{
-		for (int j = 0; j < N; j++)
-		{
-			if (M2[i][j] != 1) return true;
-		}
+		MPI_Recv(matrix[i], N, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
 	}
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			if (M3[i][j] != 1) return true;
-		}
-	}
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			if (M4[i][j] != 1) return true;
-		}
-	}
-	return false;
 }
